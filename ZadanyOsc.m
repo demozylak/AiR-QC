3c501a4e0f748b308d99eaa7ced203034203f1e0
Marcin Zyla <marcin.zyla0@gmail.com>
2017-11-25 00:56:50 +0100

    Fix for ZadOsc

--- ZadanyOsc.m
+++ ZadanyOsc.m
@@ -10,7 +10,7 @@ function [ k ] = ZadanyOsc( K_otw, st_osc, epsilon )
     [R,k_w] = rlocus(K_otw);
     i = 1;
     while true
-        [ ~ , st ] = max(imag(R(:,i))./real(R(:,i))); % stopien oscylacyjnosci
+        st = max(imag(R(:,i))./real(R(:,i))); % stopien oscylacyjnosci
         if st > st_osc
             k_a = k_w(i-1);
             k_b = k_w(i);
@@ -25,7 +25,7 @@ function [ k ] = ZadanyOsc( K_otw, st_osc, epsilon )
         srodek = (k_a + k_b)/2;
         R = rlocus(K_otw, srodek);
         
-        [ ~ , st ] = max(imag(R)./real(R));
+        st = max(imag(R)./real(R));
         if st > st_osc %wartosc szukana jest w lewej czesci
             k_b = srodek;
         else %wartosc szukana jest w prawej czesci
